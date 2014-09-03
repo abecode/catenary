54131cdb16d89e09bb43557d8c35dcfc192aa3c3
Abe Kazemzadeh <abe.kazemzadeh@gmail.com>
2014-09-03 00:00:32 -0700

    plot chains of different weights - still need to account for water depth!

--- octave/catenary.m
+++ octave/catenary.m
@@ -1,22 +1,60 @@
+% Prevent Octave from thinking that this
+% is a function file:
+1;
+
+function [x, y] = catenary(s, T, lambda) 
+  % s, vector of incremental length of chain (meters) 
+  % lambda,  weight per meter of chain (kg/meter)
+  % T tension from wind and wave force on boat (kg m s^-2)
+
+  g = 9.8 % earth's gravitational constant (m s^-2)
+  
+  % a, a parameter describing the source tension T to the pull of gravity
+  a = T/(lambda*g)
+
+  % dx and dy, the incremental changes in x and y, as in calculus (scaled by ds ds)
+  ds = [1 diff(s)];  % size of link. this could handle chains with differing link sizes
+  dx = a./sqrt(a**2+s.**2).*ds; % change in x relative to ds
+  dy = s./sqrt(a**2+s.**2).*ds; % change in y relative to ds
+
+  % the nifty upper triangular matrix for summation of dx and dy
+  nifty = triu(ones(length(s)));
+
+  x = dx*nifty;
+  y = dy*nifty;
+
+end
 % s, the length of the chain in increments up to 100 meters
 
 s = [0:1:100]; % meters
 
-% a, a parameter describing the source tension T to the pull of gravity
-lambda = 1 % weight per meter of chain (kg/meter)
-T = 10 % wind and wave force on boat (kg m s^-2)
-g = 9.8 % earth's gravitational constant (m s^-2)
-a = T/(lambda*g)
-
-% dx and dy, the incremental changes in x and y, as in calculus (actually per dx/ds, dy/ds)
-dx = a./sqrt(a**2+s.**2);
-dy = s./sqrt(a**2+s.**2);
-
-% the nifty upper triangular matrix for summation of dx and dy
-nifty = triu(ones(length(s)));
-
-x = dx*nifty;
-y = dy*nifty;
 figure;
+
+subplot(4,1,1)
+lambda = .01
+T = 10
+[x, y] = catenary(s, T, lambda);
 plot(x,y) 
 axis([0 100 0 100])
+
+subplot(4,1,2)
+lambda = .1
+T = 10
+[x, y] = catenary(s, T, lambda);
+plot(x,y) 
+axis([0 100 0 100])
+
+subplot(4,1,3)
+lambda = 1
+T = 10
+[x, y] = catenary(s, T, lambda);
+plot(x,y) 
+axis([0 100 0 100])
+
+subplot(4,1,4)
+lambda = 10
+T = 10
+[x, y] = catenary(s, T, lambda);
+plot(x,y) 
+axis([0 100 0 100])
+
